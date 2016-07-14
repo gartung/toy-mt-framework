3f8f418507d11aa483949215974a31464b68b20c
Patrick Gartung <gartung@fnal.gov>
2016-07-14 20:05:54 +0000

    single threaded is a special case

---
+++ cms-data/run_job_single.sh
@@ -0,0 +1,1 @@
+qsub -n 2 --mode c64 -t 120 --env LD_LIBRARY_PATH=/soft/compilers/gcc/4.8.4/lib/gcc/powerpc64-bgq-linux/4.8.4/:/soft/compilers/gcc/4.8.4/lib/gcc/:/soft/compilers/gcc/4.8.4/lib/gcc/powerpc64-bgq-linux/4.8.4/../../../../powerpc64-bgq-linux/lib/:$LD_LIBRARY_PATH ~/altbuild/BuildProducts/bin/TBBDemo ~/toy-mt-framework/cms-data/reco_hipileup_5_2_0_busywait_perfectIO_single.config
