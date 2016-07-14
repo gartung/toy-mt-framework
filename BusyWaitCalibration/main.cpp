1633583f7f4ca6e52f422e58283324058a843c28
Patrick Gartung <gartung@fnal.gov>
2016-07-14 15:39:14 +0000

    move calibrate() to a header so it can be used in other main.cpp

--- BusyWaitCalibration/main.cpp
+++ BusyWaitCalibration/main.cpp
@@ -6,45 +6,8 @@
 //  Copyright 2011 FNAL. All rights reserved.
 //
 
-#include <iostream>
-#if defined(__APPLE__)
-#include <time.h>
-#else
-#include <sys/time.h>
-#include <sys/resource.h>
-#endif
-#include "busyWait.h"
-#include <errno.h>
+#include "calibrate.h"
 
-static int calibrate(unsigned long long iLoop)
-{
-  struct timeval startCPUTime;
-  
-  rusage theUsage;
-  if(0 != getrusage(RUSAGE_SELF, &theUsage)) {
-    std::cerr<<errno;
-    return 1;
-  }
-  startCPUTime.tv_sec =theUsage.ru_stime.tv_sec+theUsage.ru_utime.tv_sec;
-  startCPUTime.tv_usec =theUsage.ru_stime.tv_usec+theUsage.ru_utime.tv_usec;
-  
-  std::cout << demo::busyWait(iLoop)<<std::endl;
-  
-  if(0 != getrusage(RUSAGE_SELF, &theUsage)) {
-    std::cerr<<errno;
-    return 1;
-  }
-  
-  
-  double const microsecToSec = 1E-6;
-  
-  double time = theUsage.ru_stime.tv_sec + theUsage.ru_utime.tv_sec - startCPUTime.tv_sec +
-  microsecToSec * (theUsage.ru_stime.tv_usec + theUsage.ru_utime.tv_usec - startCPUTime.tv_usec);
-  
-  std::cout <<iLoop<<" " << time<<" "<<iLoop/time<<std::endl;
-
-  return 0;
-}
 
 int main (int argc, const char * argv[])
 {
