ff39463d66e63c1e698a188b777deea8c7f98b26
Patrick Gartung <gartung@fnal.gov>
2016-09-09 14:42:02 -0700

    merge

--- cms-data/generate_config_cori.sh
+++ cms-data/generate_config_cori.sh
@@ -5,7 +5,7 @@ stepsize=64
 declare -i nsteps
 nsteps=32
 declare -i iterations
-iterations=1000
+iterations=200
 declare -i ncore
 ncore=2
 declare -i nSimulEvt
@@ -13,7 +13,7 @@ nSimulEvt=4
 declare -i nThreads
 nThreads=4
 declare -i busyWaitScaleFactor
-busyWaitScaleFactor=423729000
+busyWaitScaleFactor=4.23729e+08
 
 for ((x=1;x<${nsteps};x+=1)); do
 echo $x
@@ -23,11 +23,14 @@ echo "#!/bin/bash -l     "  >srun-driver-${nSimulEvtP}s.${nThreadsP}t.${iteratio
 echo "                   " >>srun-driver-${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
 echo "#SBATCH -p regular   " >>srun-driver-${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh   
 echo "#SBATCH -N 1      " >>srun-driver-${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh   
-echo "#SBATCH -t 03:00:00" >>srun-driver-${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh  
+echo "#SBATCH -t 24:00:00" >>srun-driver-${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh  
 echo "#SBATCH -J my_job  " >>srun-driver-${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh   
 echo "#SBATCH -L SCRATCH " >>srun-driver-${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
+
 sed -e "s/\$iterations/$iterations/" -e "s/\$nSimulEvt/$nSimulEvt/" -e "s/\$nThreads/$nThreads/" -e "s/\$busyWaitScaleFactor/$busyWaitScaleFactor/"  reco_hipileup_5_2_0_busywait_perfectIO.config.tt >reco_hipileup_5_2_0_busywait_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config
-echo srun -n 1 -c 64 /global/homes/g/gartung/build/BuildProducts/bin/TBBDemo /global/homes/g/gartung/toy-mt-framework/cms-data/reco_hipileup_5_2_0_busywait_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config >>srun-driver-${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
+
+echo srun -n1 -c64 /global/homes/g/gartung/build/BuildProducts/bin/TBBDemo /global/homes/g/gartung/toy-mt-framework/cms-data/reco_hipileup_5_2_0_busywait_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config >>srun-driver-${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
+
 nSimulEvt+=${ncore}
 nThreads+=${ncore}
 done
--- cms-data/generate_config_cori_single_t.sh
+++ cms-data/generate_config_cori_single_t.sh
@@ -13,8 +13,7 @@ nSimulEvt=1
 declare -i nThreads
 nThreads=1
 #nThreads=64
-declare -i busyWaitScaleFactor
-busyWaitScaleFactor=423729000
+busyWaitScaleFactor=4.23729e+08
 
 
 nSimulEvtP=`printf "%02d" $nSimulEvt`
@@ -22,40 +21,18 @@ nThreadsP=`printf "%02d" $nThreads`
 
 sed -e "s/\$iterations/$iterations/" -e "s/\$nSimulEvt/$nSimulEvt/" -e "s/\$nThreads/$nThreads/" -e "s/\$busyWaitScaleFactor/$busyWaitScaleFactor/"  reco_hipileup_5_2_0_busywait_perfectIO.config.tt >reco_hipileup_5_2_0_busywait_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config
 
+echo "#" >srun-driver.sh
 for ((x=2;x<=64;x+=2)); do
+   echo "sbatch -N1 srun-driver-${x}.sh" >>srun-driver.sh
    echo "#!/bin/bash -l     "  >srun-driver-${x}.sh  
    echo "                   " >>srun-driver-${x}.sh
    echo "#SBATCH -p regular " >>srun-driver-${x}.sh   
    echo "#SBATCH -N 1      "  >>srun-driver-${x}.sh   
-   echo "#SBATCH -t 02:00:00" >>srun-driver-${x}.sh  
+   echo "#SBATCH -t 24:00:00" >>srun-driver-${x}.sh  
    echo "#SBATCH -J my_job  " >>srun-driver-${x}.sh   
    echo "#SBATCH -L SCRATCH " >>srun-driver-${x}.sh
-   echo "srun $PWD/driver-$x.sh" >>srun-driver-${x}.sh
-  echo "#!/bin/bash" >driver-$x.sh
-  for ((y=1;y<=${x};y+=1));do
-    echo /global/homes/g/gartung/build/BuildProducts/bin/TBBDemo /global/homes/g/gartung/toy-mt-framework/cms-data/reco_hipileup_5_2_0_busywait_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config \>$x-$y.log 2\>\&1\& >>driver-$x.sh
-    echo ids[$y]=\$\! >>driver-$x.sh
+   for ((y=1;y<=${x};y+=1));do
+    echo srun -n1 -c1 /global/homes/g/gartung/build/BuildProducts/bin/TBBDemo /global/homes/g/gartung/toy-mt-framework/cms-data/reco_hipileup_5_2_0_busywait_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config\& >>srun-driver-$x.sh
     done
-  cat >> driver-$x.sh << EOF
-    tim=0
-    live=1
-    while [ \$live -eq 1 ]; do
-       let tim=\$tim+100
-       live=0
-       sleep 100
-       for ((x=1;x<$y;x+=1)); do
-          if ps -p \${ids[\$x]}> /dev/null 2>&1
-          then
-             if [ \$tim -gt 720000 ] ; then
-                kill \${ids[\$x]}
-                echo "APPLICATION RAN OUT OF TIME"
-             else
-                live=1
-             fi
-          fi
-       done
-    done
-EOF
-chmod +x driver-$x.sh
-
+echo "wait">>srun-driver-${x}.sh
 done
