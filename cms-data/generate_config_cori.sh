2662a02d3e91d29cd98fbd7d93dfe55ce39f4fe3
Patrick Gartung <gartung@fnal.gov>
2016-09-21 08:46:35 -0700

    updates from nersc

--- cms-data/generate_config_cori.sh
+++ cms-data/generate_config_cori.sh
@@ -18,7 +18,7 @@ declare -i nProcs
 #nProcs=64/nThreads
 nProcs=1
 
-echo "#">srun-driver-nt.sh
+echo "#">srun-driver-sleeping-nt.sh
 for ((x=4;x<=48;x+=4)); do
 echo $x
 nSimulEvt=$x
@@ -27,20 +27,19 @@ nThreads=$x
 nThreadsP=`printf "%02d" $nThreads`
 #nProcs=64/$x
 nProcsP=`printf "%02d" $nProcs`
-echo "#!/bin/bash -l     "  >srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh  
-echo "#SBATCH -p regular " >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh   
-echo "#SBATCH --nodes=1  " >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh   
-echo "#SBATCH --cpus-per-task=$x " >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
-echo "#SBATCH --exclusive " >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
-echo "#SBATCH --ntasks-per-node=$nProcs " >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
-echo "#SBATCH -t 01:00:00" >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh  
-echo "#SBATCH -J my_job  " >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh   
-echo "#SBATCH -L SCRATCH " >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
-echo "module swap PrgEnv-intel PrgEnv-gnu" >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
+echo "#!/bin/bash -l     "  >srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh  
+echo "#SBATCH -p regular " >>srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh   
+echo "#SBATCH --nodes=1  " >>srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh   
+echo "#SBATCH --cpus-per-task=$x " >>srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
+echo "#SBATCH --exclusive " >>srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
+echo "#SBATCH --ntasks-per-node=$nProcs " >>srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
+echo "#SBATCH -t 01:00:00" >>srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh  
+echo "#SBATCH -J reco_highpileup_sleeping_perfectIO  " >>srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh   
+echo "#SBATCH -L SCRATCH " >>srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
+echo "module swap PrgEnv-intel PrgEnv-gnu" >>srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
 
 sed -e "s/\$iterations/$iterations/" -e "s/\$nSimulEvt/$nSimulEvt/" -e "s/\$nThreads/$nThreads/" -e "s/\$busyWaitScaleFactor/$busyWaitScaleFactor/"  reco_hipileup_5_2_0_sleeping_perfectIO.config.tt >reco_hipileup_5_2_0_sleeping_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config
 
-echo srun -n1 -c$x /global/homes/g/gartung/build/BuildProducts/bin/TBBDemo /global/homes/g/gartung/toy-mt-framework/cms-data/reco_hipileup_5_2_0_sleeping_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config \& >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
-echo "wait" >>srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh 
-echo sbatch srun-driver-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh >>srun-driver-nt.sh   
+echo srun -n1 -c$x /global/homes/g/gartung/build/BuildProducts/bin/TBBDemo /global/homes/g/gartung/toy-mt-framework/cms-data/reco_hipileup_5_2_0_sleeping_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config >>srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh
+echo sbatch srun-driver-sleeping-${nProcsP}p.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.sh >>srun-driver-sleeping-nt.sh   
 done
--- cms-data/generate_config_cori_single_t.sh
+++ cms-data/generate_config_cori_single_t.sh
@@ -21,20 +21,19 @@ nThreadsP=`printf "%02d" $nThreads`
 
 sed -e "s/\$iterations/$iterations/" -e "s/\$nSimulEvt/$nSimulEvt/" -e "s/\$nThreads/$nThreads/" -e "s/\$busyWaitScaleFactor/$busyWaitScaleFactor/"  reco_hipileup_5_2_0_sleeping_perfectIO.config.tt >reco_hipileup_5_2_0_sleeping_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config
 
-echo "#" >srun-driver.sh
+echo "#" >srun-driver-sleeping.sh
 for ((x=4;x<=48;x+=4)); do
-   echo $x
-   echo "sbatch -N1 srun-driver-${x}.sh">>srun-driver.sh
-echo "#!/bin/bash -l ">srun-driver-${x}.sh
-echo "#SBATCH -p regular ">>srun-driver-${x}.sh
-echo "#SBATCH --nodes=1  ">>srun-driver-${x}.sh
-echo "#SBATCH --cpus-per-task=1 ">>srun-driver-${x}.sh
-echo "#SBATCH --exclusive ">>srun-driver-${x}.sh
-echo "#SBATCH --ntasks-per-node=$x ">>srun-driver-${x}.sh
-echo "#SBATCH -t 01:00:00  ">>srun-driver-${x}.sh
-echo "#SBATCH -J single_thread     ">>srun-driver-${x}.sh
-echo "#SBATCH -L SCRATCH ">>srun-driver-${x}.sh
-echo "module swap PrgEnv-intel PrgEnv-gnu">>srun-driver-${x}.sh
-echo "srun -n${x} /global/homes/g/gartung/build/BuildProducts/bin/TBBDemo /global/homes/g/gartung/toy-mt-framework/cms-data/reco_hipileup_5_2_0_sleeping_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config\& >>srun-driver-$x.sh
-echo "wait" ">>srun-driver-${x}.sh
+echo ${x}
+echo "sbatch -N1 srun-driver-sleeping-${x}.sh">>srun-driver-sleeping.sh
+echo "#!/bin/bash -l ">srun-driver-sleeping-${x}.sh
+echo "#SBATCH -p regular ">>srun-driver-sleeping-${x}.sh
+echo "#SBATCH --nodes=1  ">>srun-driver-sleeping-${x}.sh
+echo "#SBATCH --cpus-per-task=1 ">>srun-driver-sleeping-${x}.sh
+echo "#SBATCH --exclusive ">>srun-driver-sleeping-${x}.sh
+echo "#SBATCH --ntasks-per-node=$x ">>srun-driver-sleeping-${x}.sh
+echo "#SBATCH -t 01:00:00  ">>srun-driver-sleeping-${x}.sh
+echo "#SBATCH -J reco_hipileup_sleeping_perfectIO_Nsingle">>srun-driver-sleeping-${x}.sh
+echo "#SBATCH -L SCRATCH ">>srun-driver-sleeping-${x}.sh
+echo "module swap PrgEnv-intel PrgEnv-gnu">>srun-driver-sleeping-${x}.sh
+echo "srun -n${x} /global/homes/g/gartung/build/BuildProducts/bin/TBBDemo /global/homes/g/gartung/toy-mt-framework/cms-data/reco_hipileup_5_2_0_sleeping_perfectIO.${nSimulEvtP}s.${nThreadsP}t.${iterations}i.config" >>srun-driver-sleeping-${x}.sh
 done
