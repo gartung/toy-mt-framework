ddb6fb887f783c96a361928e44e3b7e8678e47e3
Patrick Gartung <gartung@fnal.gov>
2016-09-24 15:56:21 +0000

    more vesta

--- cms-data/generate_config.sh
+++ cms-data/generate_config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 declare -i stepsize
-stepsize=64
+stepsize=100
 declare -i nsteps
-nsteps=20
+nsteps=16
 declare -i iterations
-iterations=1000
+iterations=100
 declare -i ncore
 ncore=4
 declare -i nSimulEvt
@@ -15,11 +15,13 @@ nThreads=64
 declare -i busyWaitScaleFactor
 busyWaitScaleFactor=2080000
 
-for ((x=1;x<=${nsteps};x+=1)); do
-     echo $x
-     sed -e "s/\$iterations/$iterations/" -e "s/\$nSimulEvt/$nSimulEvt/" -e "s/\$nThreads/$nThreads/" -e "s/\$busyWaitScaleFactor/$busyWaitScaleFactor/"  reco_hipileup_5_2_0_busywait_perfectIO.config.tt >reco_hipileup_5_2_0_busywait_perfectIO.${nSimulEvt}s.${nThreads}t.${iterations}i.config
+echo "#!/bin/sh" >runjob-driver.sh
 
-    iterations+=${step}
-    nSimulEvt+=${ncore}
-    nThreads+=${ncore}
+for ((x=1;x<=${nsteps};x+=1)); do
+echo $x
+echo qsub -n 32 --mode c1  -t 120 --env LD_LIBRARY_PATH=/soft/compilers/gcc/4.8.4/lib/gcc/powerpc64-bgq-linux/4.8.4/:/soft/compilers/gcc/4.8.4/lib/gcc/:/soft/compilers/gcc/4.8.4/lib/gcc/powerpc64-bgq-linux/4.8.4/../../../../powerpc64-bgq-linux/lib/:\$LD_LIBRARY_PATH ~/altbuild/BuildProducts/bin/TBBDemo ~/toy-mt-framework/cms-data/reco_hipileup_5_2_0_busywait_perfectIO.${nSimulEvt}s.${nThreads}t.${iterations}i.config >>driver-$nsteps.sh
+echo runjob -n 32 -p1  --block \$COBALT_PARTNAME --verbose=DEBUG --envs LD_LIBRARY_PATH=/soft/compilers/gcc/4.8.4/lib/gcc/powerpc64-bgq-linux/4.8.4/:/soft/compilers/gcc/4.8.4/lib/gcc/:/soft/compilers/gcc/4.8.4/lib/gcc/powerpc64-bgq-linux/4.8.4/../../../../powerpc64-bgq-linux/lib/:\$LD_LIBRARY_PATH --exe ~/altbuild/BuildProducts/bin/TBBDemo --args ~/toy-mt-framework/cms-data/reco_hipileup_5_2_0_busywait_perfectIO.${nSimulEvt}s.${nThreads}t.${iterations}i.config >>runjob-driver-${nsteps}.sh
+iterations+=${stepsize}
+nSimulEvt+=${ncore}
+#nThreads+=${ncore}
 done
