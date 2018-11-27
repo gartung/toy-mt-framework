008f258b71bba31c4c64685fce28e45d0451f53c
Patrick Gartung <gartung@fnal.gov>
2018-11-27 22:54:11 +0000

    update generateConfig.py script to output a config file

--- cms-data/generateConfig.py
+++ cms-data/generateConfig.py
@@ -15,8 +15,9 @@ f = file(sys.argv[1])
 if not f:
     print "unable to open file %s",sys.argv[1]
 
-moduleRelations = defaultdict(set)
-moduleConsumes = defaultdict(set)
+moduleRelations = defaultdict(list)
+moduleConsumes = defaultdict(list)
+moduleTimings = defaultdict(list)
 processName = None
 twospace=re.compile('^  \D*')
 fourspace=re.compile('^    \D*')
@@ -28,34 +29,42 @@ for l in f:
     if twospace.match(l):
         if modules.match(l):
             processName = values[1].replace("'","")
-            print "process: %s consumes the product output from modules" % processName
-            moduleRelations[processName]=set()
+#            print "process '%s' modules:" % processName
+            moduleRelations[processName]=list()
         if consumes.match(l):
             processName = values[1].replace("'","")
-            print "process: %s consumes products with labels" % processName
-            moduleConsumes[processName]=set()
+#            print "process '%s' consumes:" % processName
+            moduleConsumes[processName]=list()
     if fourspace.match(l):
         if len(values) == 7:
             if not values[-2] == 'reRECO':
-                moduleConsumes[processName].add(values[1])
+                moduleConsumes[processName].append(values[1])
+#                print "\t%s" % values
         if len(values) == 3:
             if values[0].endswith('/'):
-                moduleRelations[processName].add(values[1])
+                moduleRelations[processName].append(values[1])
+#                print "\t%s" % values
 
-print moduleRelations
-print moduleConsumes    
+#print moduleRelations
+#print storageToGet    
             
-with open('module-consumes.yaml', 'w') as outfile:
-   outfile.write(yaml.dump(moduleConsumes, default_flow_style=True))
+#with open('module-storage2get.yaml', 'w') as outfile:
+#   outfile.write(yaml.dump(storageToGet, default_flow_style=True))
 
-with open('module-relations.yaml', 'w') as outfile:
-   outfile.write(yaml.dump(moduleRelations, default_flow_style=True))
+#with open('module-relations.yaml', 'w') as outfile:
+#   outfile.write(yaml.dump(moduleRelations, default_flow_style=True))
 
 with open('module-timings.yaml', 'r') as infile:
     moduleTimings=yaml.load(infile)
 
+storageToGet=set()
+for mod,consumes in moduleConsumes.items():
+    for c in consumes:
+        storageToGet.add(c)
+
+#storageToGet=list(storageToGet)
+
 nEvents="100"
-storageToGet=""
 
 config = {
  "process" :
@@ -91,5 +100,37 @@ config = {
  }
 }
 
+#add producers
+producers = config["process"]["producers"]
+for mod,dependents in moduleRelations.items():
+    moduleLabel = mod
+    time = moduleTimings.get(moduleLabel,[0.])
+    toGet = list()
+    for d in dependents:
+        toGet.append({"label":d, "product":""})
+    c = { "@label" : moduleLabel,
+      "@type" : "demo::EventTimesBusyWaitProducer",
+      "threadType" : "ThreadSafeBetweenInstances",
+      "eventTimes":time,
+      "toGet" :toGet
+    }
+    producers.append(c)
+
+
+
+with open('config.yaml', 'w') as outfile:
+   outfile.write(yaml.dump(config, default_flow_style=True))
+
+import pprint
+import StringIO
+output = StringIO.StringIO()
+#pp = pprint.PrettyPrinter(indent=3)
+pprint.pprint(config,stream=output)
+
+#have to convert single quotes to double quotes
+configString = output.getvalue()
+configString = configString.replace("'",'"')
+
+print configString
 
 
