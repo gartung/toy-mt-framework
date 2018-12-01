16d7f7dff075839a8b1159f22cdd6f096f187a4a
Patrick Gartung <gartung@fnal.gov>
2018-11-30 20:34:49 -0600

    update from RECO tracer log instead of reRECO tracer log

--- cms-data/generateConfig.py
+++ cms-data/generateConfig.py
@@ -7,14 +7,14 @@ from collections import defaultdict
 import pprint
 import StringIO
 
-#if len(sys.argv) != 2:
-#    print "requires input with module relations from tracer log"
-#    sys.exit(1)
-#
-#f = file(sys.argv[1])
-#if not f:
-#    print "unable to open file %s",sys.argv[1]
-#
+if len(sys.argv) != 2:
+    print "requires input with module relations from tracer log"
+    sys.exit(1)
+
+f = file(sys.argv[1])
+if not f:
+    print "unable to open file %s",sys.argv[1]
+
 moduleRelations = defaultdict(list)
 moduleConsumes = defaultdict(list)
 moduleTimings = defaultdict(list)
@@ -24,43 +24,43 @@ fourspace=re.compile('^    \D*')
 modules=re.compile('.*modules:$')
 consumes=re.compile('.*consumes:$')
 
-#for l in f:
-#    values = l.split("'")
-#    if twospace.match(l):
-#        if modules.match(l):
-#            processName = values[1].replace("'","")
-##            print "process '%s' modules:" % processName
-#            moduleRelations[processName]=list()
-#        if consumes.match(l):
-#            processName = values[1].replace("'","")
-##            print "process '%s' consumes:" % processName
-#            moduleConsumes[processName]=list()
-#    if fourspace.match(l):
-#        if len(values) == 7:
-#            if not values[-2] == 'reRECO':
-#                moduleConsumes[processName].append(values[1])
-##                print "\t%s" % values
-#        if len(values) == 3:
-#            if values[0].endswith('/'):
-#                moduleRelations[processName].append(values[1])
-##                print "\t%s" % values
-#
-#            
-#with open('module-storage2get.yaml', 'w') as outfile:
-#   outfile.write(yaml.dump(moduleConsumes, default_flow_style=True))
-#
-#with open('module-relations.yaml', 'w') as outfile:
-#   outfile.write(yaml.dump(moduleRelations, default_flow_style=True))
-#
+for l in f:
+    values = l.split("'")
+    if twospace.match(l):
+        if modules.match(l):
+            processName = values[1].replace("'","")
+#            print "process '%s' modules:" % processName
+            moduleRelations[processName]=list()
+        if consumes.match(l):
+            processName = values[1].replace("'","")
+#            print "process '%s' consumes:" % processName
+            moduleConsumes[processName]=list()
+    if fourspace.match(l):
+        if len(values) == 7:
+            if not values[-2] == 'reRECO':
+                moduleConsumes[processName].append(values[1])
+#                print "\t%s" % values
+        if len(values) == 3:
+            if values[0].endswith('/'):
+                moduleRelations[processName].append(values[1])
+#                print "\t%s" % values
+
+            
+with open('module-storage2get.yaml', 'w') as outfile:
+   outfile.write(yaml.dump(moduleConsumes, default_flow_style=True))
+
+with open('module-relations.yaml', 'w') as outfile:
+   outfile.write(yaml.dump(moduleRelations, default_flow_style=True))
+
 with open('module-timings.yaml', 'r') as infile:
     moduleTimings=yaml.load(infile)
 
-with open('module-storage2get.yaml', 'r') as infile:
-    moduleConsumes=yaml.load(infile)
-
-with open('module-relations.yaml', 'r') as infile:
-    moduleRelations=yaml.load(infile)
-
+#with open('module-storage2get.yaml', 'r') as infile:
+#    moduleConsumes=yaml.load(infile)
+#
+#with open('module-relations.yaml', 'r') as infile:
+#    moduleRelations=yaml.load(infile)
+#
 
 storageToGet=set()
 for mod,consumes in moduleConsumes.items():
